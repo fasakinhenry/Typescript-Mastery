ca987f8f7ac3a0f2c47c0e08892d5a51c2a3421a
Fasakin Henry <fasakinhenry@gmail.com>
2025-10-19 22:37:37 +0100

    feat: add the main.ts file
    also updates the lesson 02 README

--- lesson05-Type_Casting/src/main.ts
+++ lesson05-Type_Casting/src/main.ts
@@ -23,3 +23,16 @@ let myVal: string = addOrConcat(2, 2, 'concat') as string;
 
 // Be careful! TS sees no problem - but a string is returned
 let nextVal: number = addOrConcat(2, 2, 'concat') as number;
+
+// 10 as String // This will give Error
+// Double casting or Forced casting
+10 as unknown as string;
+
+// Types on the DOM - Document Object Model
+const img = document.querySelector('img')!;
+const myImg = document.getElementById('#img') as HTMLImageElement;
+const nextImg = <HTMLImageElement>document.getElementById('#img');
+
+img.src
+myImg.src
+nextImg.src
